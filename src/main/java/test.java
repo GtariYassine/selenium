e5dfc7f2a932ac0ffeadf006b493931f11844981
GtariYassine <gtari.yassine.2lfig03@gmail.com>
2021-01-07 20:54:33 +0100

    Initial commit

---
+++ src/main/java/test.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class test {
+
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
+
+        DesiredCapabilities cap = DesiredCapabilities.chrome();
+        URL url=new URL("http://localhost:4446/wd/hub");
+        RemoteWebDriver driver=new RemoteWebDriver(url,cap);
+        driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
+
+        driver.get("https://www.google.com/");
+        Thread.sleep(20000);
+        System.out.println(driver.getTitle());
+
+
+        driver.close();
+        driver.quit();
+
+
+    }
+
+
+}
